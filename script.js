0bd52647adcd0e32b9b8130a5860c70ab29954fa
Sainathreddy <sai65265@gmail.com>
2025-07-05 22:12:05 +0530

    added automatic year update to script.js

--- script.js
+++ script.js
@@ -37,3 +37,5 @@ document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
 });
+
+document.getElementById("year").textContent = new Date().getFullYear();
